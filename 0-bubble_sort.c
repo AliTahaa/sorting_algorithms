e8327514d3ea0a50677b0fbea0496cdbb7f91168
AliTahaa <ali.mohamed.taha.h@gmail.com>
2024-04-30 17:43:12 +0300

    bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,13 +11,13 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 	size_t i, j;
 
-	if (size < 2)
+	if (!array || !size)
 		return;
 
 	for (i = 0; i < size; i++)
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size - 1; j++)
 		{
-			if (array[j] > array[j + 1] && array[j + 1])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
